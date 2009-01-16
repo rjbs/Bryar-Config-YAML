a3817172239cd5737f507c722bb97c9e7eaee5f1
Ricardo SIGNES <rjbs@cpan.org>
2009-01-16 09:59:19 -0500

    prep new release

--- lib/Bryar/Config/YAML.pm
+++ lib/Bryar/Config/YAML.pm
@@ -12,11 +12,11 @@ Bryar::Config::YAML - Bryar configuration stored in YAML
 
 =head1 VERSION
 
-version 0.102
+version 0.103
 
 =cut
 
-our $VERSION = '0.102';
+our $VERSION = '0.103';
 
 =head1 SYNOPSIS
 
